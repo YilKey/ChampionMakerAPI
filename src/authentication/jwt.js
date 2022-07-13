0fddb94d7e9f3de1c476be1963fbbbff1c9ec5a3
Kerem Yilmaz <kerem.yilmaz@student.hogent.be>
2022-07-13 17:40:05 +0200

    Jwt token extra information Now the loged in username is in the jwt token

--- src/authentication/jwt.js
+++ src/authentication/jwt.js
@@ -10,6 +10,7 @@ const JWT_EXPIRATION_INTERVAL = config.get("auth.jwt.expirationInterval");
 module.exports.generateJWT = (user) => {
   const tokenData = {
     userId: user.id,
+    name: user.userName,
     roles: user.roles,
   };
 
