374d2e6bcf87ea15bd7206ea15ba77bec43dcaaa
Kerem Emre Yilmaz <79158531+YilKey@users.noreply.github.com>
2022-07-26 12:28:43 +0200

    Typo

--- config/production.js
+++ config/production.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   cors: {
     origins: ["http://localhost:3000",
-             "https://yilkey.github.io/"],
+             "https://yilkey.github.io/ChampionMakerFront"],
     maxAge: 3 * 60 * 60,
   },
   database: {
